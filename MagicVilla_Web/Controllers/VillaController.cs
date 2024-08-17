bc589bcd2da077c870403c7dca85b1e5eae6e270
Giovanni Di Guardo <giovdigua@gmail.com>
2024-08-17 20:21:22 +0200

    Finish section 7

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -42,10 +42,11 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.CreateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa created successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
-
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -70,10 +71,11 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa update successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
-
+            TempData["error"] = "Error encountered";
             return View(model);
         }
 
@@ -97,9 +99,10 @@ namespace MagicVilla_Web.Controllers
             var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
             if (response != null && response.IsSuccess)
             {
+                TempData["success"] = "Villa delete successfully";
                 return RedirectToAction(nameof(IndexVilla));
             }
-
+            TempData["error"] = "Error encountered";
             return View(model);
         }
     }
